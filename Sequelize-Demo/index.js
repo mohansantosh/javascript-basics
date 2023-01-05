51ef3bbb8c9c03e6062149cda27de1788237252b
Mohan Santosh <bheesettymohan@gmail.com>
2023-01-05 10:16:33 +0530

    add one-to-many and many-to-many relationship examples

--- Sequelize-Demo/index.js
+++ Sequelize-Demo/index.js
@@ -5,7 +5,8 @@
 //Use Models that are associated to Tables to create/update/Read/delete - CRUD
 
 const Sequelize = require('sequelize');
-const crypto = require("crypto")
+const crypto = require("crypto");
+
 const connectionObj = {
     "username": "codex",
     "password": "default@123",
@@ -88,19 +89,160 @@ let EmployeeInfo = sequelize.define('EmployeeInfo', {
 }
 );
 
+
+let EmployeeAddress = sequelize.define('EmployeeAddress', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.UUID
+    },
+    houseNo: {
+        type:  Sequelize.STRING,
+        allowNull: false
+    },
+    streetName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    city: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    district: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    state: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    addressType: {
+        type: Sequelize.DataTypes.ENUM(["work", "home", "others"])
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now')
+    }
+},
+{
+    freezeTableName: true,
+    tableName: 'employee_address'
+}
+);
+
+
+let Designation = sequelize.define('Designation', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.UUID
+    },
+    designation_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},
+{
+    freezeTableName: true,
+    tableName: 'designations'
+}
+);
+
 //Create one-to-one relationship
 Employee.hasOne(EmployeeInfo);
+EmployeeInfo.belongsTo(Employee);
+
+//Create one-to-many relationship
+EmployeeInfo.hasMany(EmployeeAddress);
+EmployeeAddress.belongsTo(EmployeeInfo);
+
+//Create many-to-many
+
+Employee.belongsToMany(Designation, {through : 'employee_designation'});
+Designation.belongsToMany(Employee, {through : 'employee_designation'});
 
 //Sync the tables between the sequelize and database;
+
+
 async function createTables(){
     try{
         await sequelize.sync({force: true});
-        console.log("Tables created in the database");
+        let employee = await createEmployee("MOHAN-123");
+        let employee1 = await createEmployee("ANJI-987");
+        //creating my information in the employees_info table
+        let employeeInfo = await createEmployeeInfo();
+        //create the one-to-one relationship between employee and employeeIno
+        await employee.setEmployeeInfo(employeeInfo);
+    
+        console.log("employee_info>>", (await employeeInfo.getEmployee()).employeeId);
+        let employeeAddress1 = await createEmployeeAddress({
+            id: crypto.randomUUID(),
+            houseNo: "123",
+            streetName: "Madhavadhara",
+            city: "Vizag",
+            district: "Visakhapatnam",
+            state: "Andhra Pradesh",
+            addressType: "home"
+        })
+    
+        let employeeAddress2 = await createEmployeeAddress({
+            id: crypto.randomUUID(),
+            houseNo: "2/4/3",
+            streetName: "Ashok Nagar",
+            city: "Sholingnallur",
+            district: "Chennai",
+            state: "TamilNadu",
+            addressType: "home"
+        })
+        //creates one-to-many relationship between employee_infoand employee_address data.
+        //or creates primary and foreign key relationship between the tables employee_info and employee_address
+        await employeeInfo.addEmployeeAddresses([employeeAddress1, employeeAddress2]);
+        await createDesignations();
+
+        //create many-to-many relationship between employees and designations
+        let designations = await Designation.findAll({limit: 2});
+        let designation = await Designation.findAll({limit: 1});
+        //employee=mohan
+        //creates records in the junction table i.e employee_designation 
+        await employee.addDesignations(designations);
+        //employee=anji
+        //creates records in the junction table i.e employee_designation 
+        await employee1.addDesignation(designation);
+        //returning employee=mohan
+        return employee;
+
     } catch(error) {
         throw new Error(error);
     }
 }
 
+
+async function createDesignations() {
+    try{
+        let designation = await Designation.create({
+            id: crypto.randomUUID(),
+            designation_name: "Software Engineer"
+        });
+        designation = await Designation.create({
+            id: crypto.randomUUID(),
+            designation_name: "Architect"
+        });
+        designation = await Designation.create({
+            id: crypto.randomUUID(),
+            designation_name: "Mentor"
+        });
+        designation = await Designation.create({
+            id: crypto.randomUUID(),
+            designation_name: "Consultant"
+        });
+    }catch(error) {
+        throw new Error(error);
+    }
+}
 
 //fn to create employee info in the table employees_info
 async function createEmployeeInfo() {
@@ -115,6 +257,15 @@ async function createEmployeeInfo() {
         });
         return employeeInfo;
 
+    } catch(error) {
+        throw new Error(error);
+    }
+}
+
+async function createEmployeeAddress(address) {
+    try {
+        let employeeAddress = await EmployeeAddress.create(address);
+        return employeeAddress;
     } catch(error) {
         throw new Error(error);
     }
@@ -135,10 +286,15 @@ async function createEmployee(employeeId) {
 
 //call the functions using promise chains
 createTables()
-.then(async (done) => {
-    let employee = await createEmployee("MOHAN-MENTOR-123");
-    let employeeInfo = await createEmployeeInfo();
-    await employee.setEmployeeInfo(employeeInfo);
+.then(async (employee) => {
+    console.log("get all designations of employee " + employee.employeeId, (await employee.getDesignations()).map((designation) => designation.designation_name));
+    // console.log("get employeess with a designation", (await designtation.getEmployees())[0].employeeId);
+
+    console.log((await (await employee.getEmployeeInfo()).getEmployeeAddresses()).length);
+    // let designation = await Designation.findAll({limit: 1});
+    // console.log(designation[0].designation_name)
+    // console.log((await designation[0].getEmployees()).map(((employee) => employee.employeeId)));
+
 })
 .catch((error) => {
     console.log("Error ", error);
